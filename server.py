63530c9c94ffba500ef468d18f0f5b4452bfcefc
jmoon9 <47996673+jmoon9@users.noreply.github.com>
2019-05-08 01:21:06 -0400

    action post request server

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 import json
+import os
+import random
 
 application = Flask(__name__)
 api = Api(application)
@@ -13,7 +15,7 @@ class Player:
         self.wager = wager
         self.move_status = move_status
         self.win_status = win_status
-        self.score = None #TODO: calculate score from hand
+        self.score = None #TODO: calculate score from hand 
 
 class _game:
     def __init__(self):
@@ -121,17 +123,200 @@ class GameStatus(Resource):
 
 api.add_resource(GameStatus, '/status')
 
-def quit_game(seatNo):
+
+
+class QuitGame(Resource):
+    def post(self):
+        global game
+        print("Action request\n")
+        
+        req = get_json(force=True)
+        handle_action(req)
+
+
+        return None
+
+api.add_resource(QuitGame, '/QG')
+
+def quit_game(req):
 
     global game
+    seatNo = req['seatNo']
 
-
-    game.playersList[seatNo].alias = None
-    game.playersList[seatNo].balance = None
+    game.playersList[seatNo].alias = Player(None, None, None, None, None, None)
 
     #edit game status json
 
+class Action(Resource):
+    def post(self):
+        global game
+        print("Action request\n")
+        
+        req = get_json(force=True)
+        handle_action(req)
 
+
+        return None
+
+api.add_resource(Action, '/AC')
+
+def handle_action(req):
+    global deck
+    global game
+    seatNo = req['seatNo']
+    game.playersList[seatNo].move_status = req['action']
+    #insert logic for move
+
+    if(game.playersList[seatNo].move_status == "HT"):
+        hit(game.playersList[seatNo].hand)
+
+        # msg = "GU:" + str(game.dealer.hand[0])      
+        # for i in range(5):
+        #     if(game.playersList[i].hand == None):
+        #         msg = msg + ":--"
+        #     else:
+        #         msg = msg + ":"
+        #         for card in game.playersList[i].hand:
+        #             msg = msg + str(card)
+        # broadcast(bytes(":" + msg, "utf8"))                       #send game update message
+
+    elif(game.playersList[seatNo].move_status == "DD"):
+        wager_int = int(game.playersList[seatNo].wager) 
+        wager_int *= 2
+        game.playersList[seatNo].wager = str(wager_int)
+        hit(game.playersList[seatNo].hand)
+        game.playersList[seatNo].move_status = "ST"
+
+        # msg = "GU:" + str(game.dealer.hand[0])
+        # for i in range(5):
+        #     if(game.playersList[i].hand == None):
+        #         msg = msg + ":--"
+        #     else:
+        #         msg = msg + ":"
+        #         for card in game.playersList[i].hand:
+        #             msg = msg + str(card)
+        # broadcast(bytes(":" + msg, "utf8"))                       #send game update message
+
+    if(game.playersList[seatNo].move_status == "ST"):
+
+        done_with_hand = "YES"
+        for i in range(5):
+            if(game.playersList[i].alias != None):
+                if(game.playersList[i].move_status != "ST"):
+                    done_with_hand = "NO"
+        if(done_with_hand != "NO"):
+            # msg = "DU:"
+            while total(game.dealer.hand) < 17:
+                hit(game.dealer.hand)
+                # for card in game.dealer.hand:
+                #     msg = msg + str(card)
+                # broadcast(bytes(":" + msg, "utf8"))
+            game.dealer.move_status = "ST"
+
+        while True:  #holds player after ST, waits for other players and dealer
+            if(game.dealer.move_status == "ST"): #check for win/loss
+                game.playersList[seatNo].win_status = score(game.dealer.hand, game.playersList[seatNo].hand)
+                game.playersList[seatNo].move_status = "Done"
+                break
+        
+        game_status = "Over"
+        for i in range(5):
+            if(game.playersList[i].move_status != "Done"):
+                if(game.playersList[i].alias != None):
+                    game_status = "Going"
+
+        if(game_status == "Over"):
+            msg = "GE"
+            game.dealer.score = total(game.dealer.hand)
+            # msg = msg + ":" + str(game.dealer.score)
+            # for i in range(5):
+            #     if(game.playersList[i].alias != None):
+            #         msg = msg + ":" + str(game.playersList[i].win_status)  
+            #     else:
+            #         msg = msg + ":"
+            # broadcast(bytes(":" + msg, "utf8"))     #send Game End W/L message to clients
+            
+            for i in range(5):              #updating player balances
+                if(game.playersList[i].alias != None):
+                    if(game.playersList[i].win_status == "W"):
+                        balance_update = int(game.playersList[i].wager)
+                        balance = int(game.playersList[i].balance)
+                        balance += balance_update
+                        game.playersList[i].balance = str(balance)
+
+                    elif(game.playersList[i].win_status == "L"):
+                        balance_update = int(game.playersList[i].wager)
+                        balance = int(game.playersList[i].balance)
+                        balance -= balance_update
+                        game.playersList[i].balance = str(balance)
+
+                game.playersList[i].hand = None             #reset player values for new game
+                game.playersList[i].wager = None
+                game.playersList[i].win_status = None
+                game.playersList[i].move_status = None
+
+            game.dealer.hand = []
+            game.dealer.move_status = None
+            deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
+
+
+
+
+
+
+
+def deal(deck):											#BTS
+    hand = []
+    for i in range(2):
+	    random.shuffle(deck)
+	    card = deck.pop()
+	    if card == 11:card = "J"
+	    if card == 12:card = "Q"
+	    if card == 13:card = "K"
+	    if card == 14:card = "A"
+	    if card == 10:card = "T"
+	    hand.append(card)
+    return hand
+
+def total(hand):
+    total = 0
+    for card in hand:
+	    if card == "T" or card == "J" or card == "Q" or card == "K":
+	        total+= 10
+	    elif card == "A":
+	        if total >= 11: total+= 1
+	        else: total+= 11
+	    else:
+	        total += card
+    return total
+
+def hit(hand):
+	card = deck.pop()
+	if card == 11:card = "J"
+	if card == 12:card = "Q"
+	if card == 13:card = "K"
+	if card == 14:card = "A"
+	if card == 10:card = "T"
+	hand.append(card)
+	return hand
+
+def score(dealer_hand, player_hand):
+    if total(player_hand) == total(dealer_hand):
+        return "P"
+    elif total(player_hand) == 21:
+        return "W"  
+    elif total(dealer_hand) == 21:
+        return "L"
+    elif total(player_hand) > 21:
+        return "L"
+    elif total(dealer_hand) > 21:
+        return "W"
+    elif total(player_hand) < total(dealer_hand):
+        return "L"
+    elif total(player_hand) > total(dealer_hand):
+        return "W"
+
+deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
 
 
 
